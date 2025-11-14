dfcfe6b43684a2e9e2127e7d271aecb8cd536145
MathiasHv18 <mathiashv2014@gmail.com>
2025-11-14 09:06:24 -0500

    Insercion de datos mongoDB completo

--- src/PC4/data_processing.go
+++ src/PC4/data_processing.go
@@ -15,19 +15,10 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func connectMongo() *mongo.Collection {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	uri := "mongodb://hello:world@localhost:27017"
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	collection := client.Database("movielens").Collection("ratings")
-	fmt.Println("✅ Conectado a MongoDB y lista la colección 'ratings'")
-	return collection
+type Movie struct{
+	MovieID int `bson:"movieId"`
+	Title string `bson:"title"`
+	Genres string `bson:"genres"`
 }
 
 type Rating struct {
@@ -41,7 +32,29 @@ const (
 	numWorkers = 10
 )
 
-func worker(id int, jobs <-chan [][]string, wg *sync.WaitGroup, collection *mongo.Collection) {
+
+func connectMongo(typeConnection int) *mongo.Collection {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	uri := "mongodb://hello:world@localhost:27017"
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if typeConnection == 1 {
+		collection := client.Database("movielens").Collection("movies")
+		fmt.Println("✅ Conectado a MongoDB y lista la colección 'movies'")
+		return collection
+	}
+	
+	collection := client.Database("movielens").Collection("ratings")
+	fmt.Println("✅ Conectado a MongoDB y lista la colección 'ratings'")
+	return collection
+}
+
+func workerRating(id int, jobs <-chan [][]string, wg *sync.WaitGroup, collection *mongo.Collection) {
 	defer wg.Done()
 	ctx := context.Background()
 
@@ -55,56 +68,99 @@ func worker(id int, jobs <-chan [][]string, wg *sync.WaitGroup, collection *mong
 		}
 
 		if len(ratings) > 0 {
-			_, err := collection.InsertMany(ctx, ratings)
-			if err != nil {
-				log.Printf("Worker %d error al insertar: %v\n", id, err)
-			} else {
-				fmt.Printf("Worker %d inserto %d registros\n", id, len(ratings))
-			}
+			collection.InsertMany(ctx, ratings)
+			fmt.Printf("Worker %d inserto %d registros en Ratings\n", id, len(ratings))
 		}
 	}
 }
 
-// Falta hacer uan funcion para que lea los csv distintos como ratinfs y movies
+func workerMovie(id int, jobs <-chan [][]string, wg *sync.WaitGroup, collection *mongo.Collection) {
+	defer wg.Done()
+	ctx := context.Background()
 
+	for records := range jobs {
+		var movies []interface{}
+		for _, record := range records {
+			movieId, _ := strconv.Atoi(record[0])
+			title := record[1]
+			genres := record[2]
+			movies = append(movies, Movie{MovieID: movieId, Title: title, Genres: genres})
+		}
+
+		if len(movies) > 0 {
+			collection.InsertMany(ctx, movies)
+			fmt.Printf("Worker %d inserto %d registros en Movies\n", id, len(movies))
+		}
+	}
+}
 
 func main() {
-	collection := connectMongo()
+	collectionMovies := connectMongo(1)
+	collectionRatings := connectMongo(2)
+	
+	fileRating, _ := os.Open("./data/ratings_clean.csv")
+	fileMovie, _ := os.Open("./data/movies_clean.csv")
 
-	file, err := os.Open("./data/ratings_clean.csv")
-	if err != nil {
-		log.Fatal("Error abriendo CSV:", err)
-	}
-	defer file.Close()
+	defer fileMovie.Close()
+	defer fileRating.Close()
 
-	reader := csv.NewReader(bufio.NewReader(file))
+	readerRating := csv.NewReader(bufio.NewReader(fileRating))
+	readerMovie := csv.NewReader(bufio.NewReader(fileMovie))
 
-	jobs := make(chan [][]string, numWorkers)
+	jobsRating := make(chan [][]string, numWorkers)
+	jobsMovie := make(chan [][]string, numWorkers)
+
 	var wg sync.WaitGroup
 
 	for i := 0; i < numWorkers; i++ {
 		wg.Add(1)
-		go worker(i, jobs, &wg, collection)
+		go workerRating(i, jobsRating, &wg, collectionRatings)
 	}
 
-	var batch [][]string
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+		go workerMovie(i, jobsMovie, &wg, collectionMovies)
+	}
+
+	var batchMovie [][]string
 	for {
-		record, err := reader.Read()
+		record, err := readerMovie.Read()
 		if err != nil {
 			break
 		}
-		batch = append(batch, record)
-		if len(batch) == chunkSize {
-			jobs <- batch
-			batch = nil
+		batchMovie = append(batchMovie, record)
+		if len(batchMovie) == chunkSize {
+			jobsMovie <- batchMovie
+			batchMovie = nil
 		}
 	}
 
-	if len(batch) > 0 {
-		jobs <- batch
+	if len(batchMovie) > 0 {
+		jobsMovie <- batchMovie
 	}
 
-	close(jobs)
+	close(jobsMovie)
+
+
+	var batchRating [][]string
+	for {
+		record, err := readerRating.Read()
+		if err != nil {
+			break
+		}
+		batchRating = append(batchRating, record)
+		if len(batchRating) == chunkSize {
+			jobsRating <- batchRating
+			batchRating = nil
+		}
+	}
+
+	if len(batchRating) > 0 {
+		jobsRating	 <- batchRating
+	}
+
+	close(jobsRating)
+
 	wg.Wait()
 	fmt.Println("Procesamiento completado")
 }
